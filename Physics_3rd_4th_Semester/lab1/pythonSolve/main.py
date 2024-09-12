62ca7f76ba8af3b63e28928bb3c13cf1908784e0
Pavel Sarancha <sarancha.pasha@mail.ru>
2024-09-12 04:24:08 +0300

    physics

---
+++ Physics_3rd_4th_Semester/lab1/pythonSolve/main.py
@@ -0,0 +1,72 @@
+# Открываем файл и считываем периоды колебаний
+import math
+
+with open('data.txt', 'r') as f:
+    periods = [float(line.strip()) for line in f]
+    periods_len = len(periods)
+
+
+def arithmetic_mean(array):
+    # Среднее арифметическое ⟨t⟩_N
+    sum = 0
+    for i in array:
+        sum += i
+    ans = sum / len(array)
+    return round(ans, 4)
+
+
+def average_deviation():
+    # t_i - ⟨t⟩_N, с
+    deviations = []
+    for i in periods:
+        deviations.append(round(i - arithmetic_mean(periods), 2))
+    return deviations
+
+
+def check_average_deviation():
+    deviations = average_deviation()
+    sum = 0
+    for i in deviations:
+        sum += i
+    return sum
+
+
+def average_deviation_pow():
+    # Отклонение периода колебаний маятника от среднего значения периода колебаний
+    # (t_i - ⟨t⟩_N )^2, с2
+    deviations = []
+    for i in periods:
+        deviations.append(round(pow(i - arithmetic_mean(periods), 2), 8))
+    return deviations
+
+
+def sample_standard_deviation():
+    # среднеквадратичное отклонение среднего значения.
+    # σ_⟨t⟩
+    arithm_m = arithmetic_mean(average_deviation_pow())
+    return math.sqrt((1 / (periods_len * (periods_len-1))) * arithm_m)
+
+
+def max_distribution_density():
+    # Максимальная плотность распределения
+    # p_max
+    return 1 / (sample_standard_deviation() * math.sqrt(2 * math.pi))
+
+
+def print_data():
+    print("Среднее арифметическое всех результатов <t>N = " + str(arithmetic_mean(periods)) + "\n")
+
+    print("t_i - <t>N = " + str(average_deviation()) + "\n")
+
+    print("(t_i - <t>N)^2 = " + str(average_deviation_pow()) + "\n")
+
+    print("σ_⟨t⟩ = " + str(sample_standard_deviation()) + "\n")
+
+    print("p_max = " + str(max_distribution_density()) + "\n")
+
+    print("Данные:" + str(periods) + "\n")
+
+
+print_data()
+
+
